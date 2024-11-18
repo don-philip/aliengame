365bf303631be230cf5e1264fbfef93530d37293
don-philip <donphilip1210@gmail.com>
2024-11-18 14:48:19 +0530

    separate setting page

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        
+        # Screen settings
+        self.screen_width = 1200   # Screen width in pixels
+        self.screen_height = 800  # Screen height in pixels
+        self.bg_color = (230, 230, 230)  # Background color (RGB tuple)
