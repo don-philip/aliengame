3ea9b85b6a79da73904e163aa72a7f88d4c485be
don-philip <donphilip1210@gmail.com>
2024-11-19 14:49:34 +0530

    added aliens

--- settings.py
+++ settings.py
@@ -8,10 +8,15 @@ class Settings:
         self.screen_width = 1200   # Screen width in pixels
         self.screen_height = 580  # Screen height in pixels
         self.bg_color = (230, 230, 230)  # Background color (RGB tuple)
-        self.ship_speed = 0.75
+        self.ship_speed = 0.50
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        # Alien settings
+        self.alien_speed = 0.10
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
