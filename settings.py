69b4dd83ac01465db4c9784a5ddc135d0be5ea9c
don-philip <donphilip1210@gmail.com>
2024-11-19 15:20:14 +0530

    removes alien and bullet on collision

--- settings.py
+++ settings.py
@@ -10,8 +10,8 @@ class Settings:
         self.bg_color = (230, 230, 230)  # Background color (RGB tuple)
         self.ship_speed = 0.50
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 2.0
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
